047fd3bff814bcadf113e276a3b271efe2a340b7
Dev <mashru.dev@gmail.com>
2020-02-15 20:00:31 +0530

    Add Beryllium

---
+++ products/beryllium/pixeldust_beryllium.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2018-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/beryllium/device.mk)
+
+# Inherit some common PixelDust stuff.
+$(call inherit-product, vendor/pixeldust/configs/pixeldust_phone.mk)
+
+# Include optional stuff (e.g. prebuilt apps)
+include vendor/pixeldust/configs/system_optional.mk
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := arrow_beryllium
+PRODUCT_DEVICE := beryllium
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := POCO F1
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := "Xiaomi/polaris/polaris:8.0.0/OPR1.170623.032/V9.5.19.0.ODGMIFA:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V11.0.4.0.QEJMIXM release-keys" \
+    PRODUCT_NAME="beryllium" \
+    TARGET_DEVICE="beryllium"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
+
+
+PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
