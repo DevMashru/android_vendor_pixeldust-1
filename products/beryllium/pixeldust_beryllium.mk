ee6071d9eabd3c91873d613f3836dc9d543bd2a7
Dev <mashru.dev@gmail.com>
2020-02-16 23:25:17 +0530

    Fix product name

--- products/beryllium/pixeldust_beryllium.mk
+++ products/beryllium/pixeldust_beryllium.mk
@@ -6,14 +6,20 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
 # Inherit some common PixelDust stuff.
 $(call inherit-product, vendor/pixeldust/configs/pixeldust_phone.mk)
 
 # Include optional stuff (e.g. prebuilt apps)
 include vendor/pixeldust/configs/system_optional.mk
 
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_beryllium
+PRODUCT_NAME := pixeldust_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
@@ -29,4 +35,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
 
 
-PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/pixeldust/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
